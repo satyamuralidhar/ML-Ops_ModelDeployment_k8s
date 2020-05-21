6237625634c4140c918be232930d41422f05b495
satyamuralidhar <muralidhar.peddireddi@gmail.com>
2020-05-21 12:48:44 +0530

    new change

--- app.py
+++ app.py
@@ -9,16 +9,16 @@ app=Flask(__name__)
 Swagger(app)
 
 pickle_in = open("model.pkl","rb")
-classifier=pickle.load(pickle_in)
+lg=pickle.load(pickle_in)
 
 @app.route('/')
 def welcome():
     return "Welcome All"
 
-@app.route('/predict',methods=["Get"])
+@app.route('/predict',methods=["GET"])
 def predict_note_authentication():
     
-    """Let's Authenticate the Banks Note 
+    """Let's Authenticate the Diabetis
     This is using docstrings for specifications.
     ---
     parameters:
@@ -67,7 +67,7 @@ def predict_note_authentication():
     BMI = request.args.get('BMI')
     DiabetesPedigreeFunction = request.args.get('DiabetesPedigreeFunction')
     Age = request.args.get('Age')
-    prediction = classifier.predict([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
+    prediction = lg.predict([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
     print(prediction)
     return "Hello The answer is"+str(prediction)
 if __name__=='__main__':
